0fa5fe6f23f7d796e860b8f9e44bfbb23e53950d
Logan Brock <lbrock@eigenx.com>
2022-08-11 14:47:58 -0400

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,29 +1,39 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `My Gatsby Site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [
+		'gatsby-plugin-sass',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-mdx',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		`gatsby-plugin-pnpm`,
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+	],
 };
 
 export default config;
